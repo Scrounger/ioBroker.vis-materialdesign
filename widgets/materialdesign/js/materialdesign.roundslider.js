c18cc99ad48ce7285ce2847fff9c3ef66d6d37e8
Scrounger <scrounger@gmx.net>
2019-11-19 17:39:39 +0100

    round slider: value label added

--- widgets/materialdesign/js/materialdesign.roundslider.js
+++ widgets/materialdesign/js/materialdesign.roundslider.js
@@ -13,17 +13,51 @@ vis.binds.materialdesign.roundslider = {
         try {
             let $this = $(el);
 
+            let valueOnLoading = vis.states.attr(data.oid + '.val');
+
+
+            let min = getNumberFromData(data.min, 0);
+            let labelMin = getValueFromData(data.valueLabelMin, null);
+            let max = getNumberFromData(data.max, 100);
+            let labelMax = getValueFromData(data.valueLabelMax, null);
+            let unit = getValueFromData(data.valueLabelUnit, '');
+
+            let valueLessThan = getNumberFromData(data.valueLessThan, min);
+            let textForValueLessThan = getValueFromData(data.textForValueLessThan, null);
+            let valueGreaterThan = getNumberFromData(data.valueGreaterThan, max);
+            let textForValueGreaterThan = getValueFromData(data.textForValueGreaterThan, null);
+
+            let labelWitdh = 0;
+            if (data.showValueLabel === true || data.showValueLabel === 'true') {
+                labelWitdh = data.valueLabelWidth;
+            }
+
             $this.append(`<round-slider class="materialdesign-round-slider-element"
-                            value=${vis.states.attr(data.oid + '.val')} 
-                            max="${getNumberFromData(data.max, 100)}" 
-                            min="${getNumberFromData(data.min, 0)}"
+                            value=${valueOnLoading} 
+                            max="${max}" 
+                            min="${min}"
                             step="${getNumberFromData(data.step, 1)}"
                             startAngle="${getNumberFromData(data.startAngle, 135)}"
                             arcLength="${getNumberFromData(data.arcLength, 270)}"
                             handleSize="${getNumberFromData(data.handleSize, 6)}"
                             handleZoom="${getNumberFromData(data.handleZoom, 1.5)}"
                             ${(data.rtl === true) ? 'rtl = "true"' : ''}
-                            ></round-slider>`)
+                            style="position: absolute;"
+                            >
+                            </round-slider>
+                            
+                            ${(data.showValueLabel) ?
+                    `<label class="labelValue" 
+                                    style="position: relative; 
+                                    text-align: center;
+                                    display: flex;
+                                    justify-content: center;
+                                    pointer-events: none;
+                                    top: ${getNumberFromData(data.valueLabelVerticalPosition, 45)}%; 
+                                    color: ${getValueFromData(data.valueLabelColor, '#1e88e5')};">
+                                        ${valueOnLoading} ${unit}
+                                </label>`: ''}
+                            `)
 
             let slider = $this.find('.materialdesign-round-slider-element');
 
@@ -46,8 +80,8 @@ vis.binds.materialdesign.roundslider = {
                 }
             });
 
-            slider.bind('value-changing', function () {
-                console.log('changing');
+            slider.bind('value-changing', function (ev) {
+                setSliderState(false, ev.target.__value);
             });
 
             slider.bind('value-changed', function (ev) {
@@ -55,11 +89,25 @@ vis.binds.materialdesign.roundslider = {
                 vis.setValue(data.oid, changedVal);
             });
 
-            function setSliderState() {
-                let val = vis.states.attr(data.oid + '.val');
-
+            function setSliderState(setVisValue = true, val = 0) {
                 if (!vis.states.attr(data.wid + '.val')) {
-                    slider.attr('value', val);
+
+                    if (setVisValue) {
+                        val = vis.states.attr(data.oid + '.val');
+                        slider.attr('value', val);
+                    }
+
+                    if (val <= min && labelMin != null) {
+                        $this.find('.labelValue').html(labelMin);
+                    } else if (val > min && val <= valueLessThan && textForValueLessThan != null) {
+                        $this.find('.labelValue').html(textForValueLessThan);
+                    } else if (val >= valueGreaterThan && val < max && textForValueGreaterThan != null) {
+                        $this.find('.labelValue').html(textForValueGreaterThan);
+                    } else if (val >= max && labelMax != null) {
+                        $this.find('.labelValue').html(labelMax);
+                    } else {
+                        $this.find('.labelValue').html(`${val} ${unit}`);
+                    }
                 }
             }
         } catch (ex) {
