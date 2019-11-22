b3d8c3e3f1299afb0db1b4061c21292d507cc2e6
Scrounger <scrounger@gmx.net>
2019-11-22 21:49:47 +0100

    version for debug changed

--- widgets/materialdesign/js/materialdesign.chart.js
+++ widgets/materialdesign/js/materialdesign.chart.js
@@ -2,7 +2,7 @@
 /*
     ioBroker.vis vis-materialdesign Widget-Set
 
-    version: "0.2.5"
+    version: "0.2.6"
 
     Copyright 2019 Scrounger scrounger@gmx.net
 */
--- widgets/materialdesign/js/materialdesign.js
+++ widgets/materialdesign/js/materialdesign.js
@@ -1,7 +1,7 @@
 /*
     ioBroker.vis vis-materialdesign Widget-Set
 
-    version: "0.2.5"
+    version: "0.2.6"
 
     Copyright 2019 Scrounger scrounger@gmx.net
 */
@@ -19,7 +19,7 @@ if (vis.editMode) {
 
 // this code can be placed directly in materialdesign.html
 vis.binds.materialdesign = {
-    version: "0.2.5",
+    version: "0.2.6",
     showVersion: function () {
         if (vis.binds["materialdesign"].version) {
             console.log('Version vis-materialdesign: ' + vis.binds["materialdesign"].version);
