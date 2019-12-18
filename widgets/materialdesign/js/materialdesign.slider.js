213da83466fb7f7e51191920c611ca50c638b4be
Scrounger <scrounger@gmx.net>
2019-12-18 17:17:56 +0100

    folder structure changed

---
+++ widgets/materialdesign/js/materialdesign.slider.js
@@ -0,0 +1,19 @@
+/*
+    ioBroker.vis vis-materialdesign Widget-Set
+
+    version: "0.2.12"
+
+    Copyright 2019 Scrounger scrounger@gmx.net
+*/
+"use strict";
+
+// this code can be placed directly in materialdesign.html
+vis.binds.materialdesign.slider = {
+    vertical: function (el, data) {
+        try {
+            
+        } catch (ex) {
+            console.exception(`vertical: error: ${ex.message}, stack: ${ex.stack}`);
+        }
+    }
+};
