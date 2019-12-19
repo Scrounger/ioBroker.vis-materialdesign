e93b480f94a5b67956d05ffa1736fb5207aa4c88
Scrounger <scrounger@gmx.net>
2019-12-19 17:44:32 +0100

    vueSliderVertical options added

--- widgets/materialdesign/js/materialdesign.slider.js
+++ widgets/materialdesign/js/materialdesign.slider.js
@@ -12,30 +12,67 @@ vis.binds.materialdesign.slider = {
     vertical: function (el, data) {
         try {
             let $this = $(el);
+            let defaultColor = '#44739e'
 
-            $this.html(`
-            <div class="slider" id="blaaaa" style="height: 100%;">
-                <v-app id="inspire" style="height: 100%;">
-                    <v-slider id="slider"
+            $this.append(`
+            <div class="vuetifySlider">
+                <v-app id="vuetifyContainer">
+                    <v-slider
                         v-model="value"
-                        vertical
-                        track-fill-color="${getValueFromData(data.colorBeforeThumb, '#44739e')}"
-                        thumb-color="${getValueFromData(data.colorThumb, '#44739e')}"
-                        track-color="${getValueFromData(data.colorAfterThumb, 'rgba(161, 161, 161, 0.26)')}"
-                        thumb-label                        
+                        :vertical="vertical"
+                        :min="min"
+                        :max="max"
+                        :step="step"
+                        :ticks="ticks"
+                        :tick-size="tickSize"
+                        :thumb-label="thumbLabel"
+                        :thumb-size="thumbSize"
+                        :loader-height="loaderHeight"
+                        :track-fill-color="trackFillColor"
+                        :thumb-color="thumbColor"
+                        :track-color="trackColor"                        
+                        always-dirty
                         @change="changeEvent"
                     >
                     </v-slider>
                 </v-app>
-          </div>`);
+            </div>`);
+
+            let showTicks = false;
+            if (getValueFromData(data.showTicks, 'no') === 'yes') {
+                showTicks = true;
+            }
+            if (getValueFromData(data.showTicks, 'no') === 'always') {
+                showTicks = 'always';
+            }
+
+            let showThumbLabel = false;
+            if (getValueFromData(data.showThumbLabel, 'no') === 'yes') {
+                showThumbLabel = true;
+            }
+            if (getValueFromData(data.showThumbLabel, 'no') === 'always') {
+                showThumbLabel = 'always';
+            }
 
             setTimeout(function () {
                 let vueSlider = new Vue({
-                    el: $this.find('#blaaaa').get(0),
+                    el: $this.find('.vuetifySlider').get(0),
                     vuetify: new Vuetify(),
                     data() {
                         return {
                             value: vis.states.attr(data.oid + '.val'),
+                            vertical: (getValueFromData(data.orientation, 'horizontal') === 'horizontal') ? false : true,
+                            min: getNumberFromData(data.min, 0),
+                            max: getNumberFromData(data.max, 100),
+                            step: getNumberFromData(data.step, 1),
+                            ticks: showTicks,
+                            tickSize: getNumberFromData(data.tickSize, 1),
+                            thumbLabel: showThumbLabel,
+                            thumbSize: getNumberFromData(data.thumbSize, 32),
+                            loaderHeight: '30px',
+                            trackFillColor: getValueFromData(data.colorBeforeThumb, defaultColor),
+                            thumbColor: getValueFromData(data.colorThumb, defaultColor),
+                            trackColor: getValueFromData(data.colorAfterThumb, 'rgba(161, 161, 161, 0.26)'),
                         }
                     },
                     methods: {
@@ -44,6 +81,16 @@ vis.binds.materialdesign.slider = {
                         }
                     }
                 })
+
+                // calculate height of Element
+                if (getValueFromData(data.orientation, 'horizontal') === 'vertical') {
+                    let height = window.getComputedStyle($this.context, null).height.replace('px', '');
+                    let sliderEl = $this.find('.v-slider--vertical');
+
+                    height = height - sliderEl.css('margin-top').replace('px', '') - sliderEl.css('margin-bottom').replace('px', '')
+                    sliderEl.css('height', height + 'px');
+                }
+
 
                 vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
                     setSliderState();
