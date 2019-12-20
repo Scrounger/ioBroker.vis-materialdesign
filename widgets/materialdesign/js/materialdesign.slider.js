207401511dc7dc1ceaf4c78a154d94c4968d1693
Scrounger <scrounger@gmx.net>
2019-12-21 00:06:58 +0100

    vueSlider: options added

--- widgets/materialdesign/js/materialdesign.slider.js
+++ widgets/materialdesign/js/materialdesign.slider.js
@@ -14,27 +14,46 @@ vis.binds.materialdesign.slider = {
             let $this = $(el);
             let defaultColor = '#44739e'
 
+            let min = getValueFromData(data.min, 0);
+            let labelMin = getValueFromData(data.valueLabelMin, null);
+            let max = getValueFromData(data.max, 100);
+            let labelMax = getValueFromData(data.valueLabelMax, null);
+            let unit = getValueFromData(data.valueLabelUnit, '');
+
+            let valueLessThan = getValueFromData(data.valueLessThan, min);
+            let textForValueLessThan = getValueFromData(data.textForValueLessThan, null);
+            let valueGreaterThan = getValueFromData(data.valueGreaterThan, max);
+            let textForValueGreaterThan = getValueFromData(data.textForValueGreaterThan, null);
+
             $this.append(`
-            <div class="vuetifySlider">
+            <div class="vuetifySlider" style="width: 100%">
                 <v-app id="vuetifyContainer">
-                    <v-slider
-                        v-model="value"
-                        :vertical="vertical"
-                        :min="min"
-                        :max="max"
-                        :step="step"
-                        :ticks="ticks"
-                        :tick-size="tickSize"
-                        :thumb-label="thumbLabel"
-                        :thumb-size="thumbSize"
-                        :loader-height="loaderHeight"
-                        :track-fill-color="trackFillColor"
-                        :thumb-color="thumbColor"
-                        :track-color="trackColor"                        
-                        always-dirty
-                        @change="changeEvent"
-                    >
-                    </v-slider>
+                    <v-container>
+                        <v-row style="align-items: center">
+                            <v-slider
+                                v-model="value"
+                                :vertical="vertical"
+                                :min="min"
+                                :max="max"
+                                :step="step"
+                                :ticks="ticks"
+                                :tick-size="tickSize"
+                                :thumb-label="thumbLabel"
+                                :thumb-size="thumbSize"
+                                :loader-height="loaderHeight"
+                                :track-fill-color="trackFillColor"
+                                :thumb-color="thumbColor"
+                                :track-color="trackColor"                        
+                                always-dirty
+                                hide-details
+                                ${(getValueFromData(data.prepandText, null) !== null) ? `label="${data.prepandText}"` : ''}
+                                @change="changeEvent"
+                                @input="inputEvent"
+                            >
+                            </v-slider>
+                            ${(data.showValueLabel) ? `<span class="slider-value" style="width: ${getNumberFromData(data.valueLabelWidth, 0)}px; text-align:right; white-space: nowrap;">0</span>` : ''}
+                        </v-row>
+                    </v-container>
                 </v-app>
             </div>`);
 
@@ -62,8 +81,8 @@ vis.binds.materialdesign.slider = {
                         return {
                             value: vis.states.attr(data.oid + '.val'),
                             vertical: (getValueFromData(data.orientation, 'horizontal') === 'horizontal') ? false : true,
-                            min: getNumberFromData(data.min, 0),
-                            max: getNumberFromData(data.max, 100),
+                            min: min,
+                            max: max,
                             step: getNumberFromData(data.step, 1),
                             ticks: showTicks,
                             tickSize: getNumberFromData(data.tickSize, 1),
@@ -75,22 +94,40 @@ vis.binds.materialdesign.slider = {
                             trackColor: getValueFromData(data.colorAfterThumb, 'rgba(161, 161, 161, 0.26)'),
                         }
                     },
+                    mounted: function () {
+                        //setSliderState();
+                    },
                     methods: {
-                        changeEvent(a) {
+                        changeEvent() {
                             vis.setValue(data.oid, this.value);
+
+                            setSliderState();
+                        },
+                        inputEvent() {
+                            setSliderState(false, this.value);
                         }
                     }
                 })
 
-                // calculate height of Element
+                // calculate width / height of Element
                 if (getValueFromData(data.orientation, 'horizontal') === 'vertical') {
                     let height = window.getComputedStyle($this.context, null).height.replace('px', '');
-                    let sliderEl = $this.find('.v-slider--vertical');
 
+                    let sliderEl = $this.find('.v-slider--vertical');
                     height = height - sliderEl.css('margin-top').replace('px', '') - sliderEl.css('margin-bottom').replace('px', '')
                     sliderEl.css('height', height + 'px');
                 }
 
+                $this.context.style.setProperty("--vue-slider-thumb-label-font-color", getValueFromData(data.thumbFontColor, ''));
+                $this.context.style.setProperty("--vue-slider-thumb-label-font-family", getValueFromData(data.thumbFontFamily, ''));
+                $this.context.style.setProperty("--vue-slider-thumb-label-font-size", getValueFromData(data.thumbFontSize, '12', '', 'px'));
+                $this.find('.v-slider__thumb-label').css('background-color', getValueFromData(data.thumbBackgroundColor, getValueFromData(data.colorThumb, defaultColor)));
+
+                $this.context.style.setProperty("--vue-slider-tick-before-color", getValueFromData(data.tickColorBefore,''));
+                $this.context.style.setProperty("--vue-slider-tick-after-color", getValueFromData(data.tickColorAfter,''));
+
+                // Slider Initialiserung setzen
+                setSliderState();
 
                 vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
                     setSliderState();
@@ -105,6 +142,23 @@ vis.binds.materialdesign.slider = {
                         if (setVisValue) {
                             val = vis.states.attr(data.oid + '.val');
                             vueSlider.value = val;
+                        }
+
+                        if (val <= min && labelMin != null) {
+                            $this.find('.slider-value').html(labelMin);
+                            if (data.useLabelRules) $this.find('.v-slider__thumb-label').find('span').html(labelMin);
+                        } else if (val > min && val <= valueLessThan && textForValueLessThan != null) {
+                            $this.find('.slider-value').html(textForValueLessThan);
+                            if (data.useLabelRules) $this.find('.v-slider__thumb-label').find('span').html(textForValueLessThan);
+                        } else if (val >= valueGreaterThan && val < max && textForValueGreaterThan != null) {
+                            $this.find('.slider-value').html(textForValueGreaterThan);
+                            if (data.useLabelRules) $this.find('.v-slider__thumb-label').find('span').html(textForValueGreaterThan);
+                        } else if (val >= max && labelMax != null) {
+                            $this.find('.slider-value').html(labelMax);
+                            if (data.useLabelRules) $this.find('.v-slider__thumb-label').find('span').html(labelMax);
+                        } else {
+                            $this.find('.slider-value').html(`${val} ${unit}`);
+                            if (data.useLabelRules) $this.find('.v-slider__thumb-label').find('span').html(`${val} ${unit}`);
                         }
                     }
                 }
