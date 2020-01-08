0980afbff1ab4ca1bc5734aa510c40fcea1477fd
Scrounger <scrounger@gmx.net>
2020-01-08 18:18:06 +0100

    mdcCheckbox moved to own file

---
+++ widgets/materialdesign/js/widgets/materialdesign.checkbox.js
@@ -0,0 +1,79 @@
+/*
+    ioBroker.vis vis-materialdesign Widget-Set
+
+    version: "0.2.34"
+
+    Copyright 2019 Scrounger scrounger@gmx.net
+*/
+"use strict";
+
+// this code can be placed directly in materialdesign.html
+vis.binds.materialdesign.checkbox =
+    function (el, data) {
+        try {
+            let $this = $(el);
+
+            let checkboxElement = $this.find('.mdc-checkbox').get(0);
+
+            const mdcFormField = new mdc.formField.MDCFormField($this.context);
+            const mdcCheckbox = new mdc.checkbox.MDCCheckbox(checkboxElement);
+            mdcFormField.input = mdcCheckbox;
+
+            mdcCheckbox.disabled = data.readOnly, false;
+
+            checkboxElement.style.setProperty("--mdc-theme-secondary", myMdwHelper.getValueFromData(data.colorCheckBox, ''));
+
+            setCheckboxState();
+
+            if (!vis.editMode) {
+                $this.find('.mdc-checkbox').click(function () {
+                    vis.binds.materialdesign.helper.vibrate(data.vibrateOnMobilDevices);
+
+                    if (data.toggleType === 'boolean') {
+                        vis.setValue(data.oid, mdcCheckbox.checked);
+                    } else {
+                        if (!mdcCheckbox.checked === true) {
+                            vis.setValue(data.oid, data.valueOff);
+                        } else {
+                            vis.setValue(data.oid, data.valueOn);
+                        }
+                    }
+
+                    setCheckboxState();
+                });
+            }
+
+            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                setCheckboxState();
+            });
+
+            function setCheckboxState() {
+                var val = vis.states.attr(data.oid + '.val');
+
+                let buttonState = false;
+
+                if (data.toggleType === 'boolean') {
+                    buttonState = val;
+                } else {
+                    if (val === parseInt(data.valueOn) || val === data.valueOn) {
+                        buttonState = true;
+                    } else if (val !== parseInt(data.valueOn) && val !== data.valueOn && val !== parseInt(data.valueOff) && val !== data.valueOff && data.stateIfNotTrueValue === 'on') {
+                        buttonState = true;
+                    }
+                }
+
+                mdcCheckbox.checked = buttonState;
+
+                let label = $this.find('label[id="label"]');
+                if (buttonState) {
+                    label.css('color', myMdwHelper.getValueFromData(data.labelColorTrue, ''));
+                    label.text(myMdwHelper.getValueFromData(data.labelTrue, ''));
+                } else {
+                    label.css('color', myMdwHelper.getValueFromData(data.labelColorFalse, ''));
+                    label.text(myMdwHelper.getValueFromData(data.labelFalse, ''));
+                }
+            }
+        } catch (ex) {
+            console.exception(`[Checkbox]: error: ${ex.message}, stack: ${ex.stack}`);
+        }
+    };
