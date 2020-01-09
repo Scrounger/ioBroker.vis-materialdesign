4ed17137a753e3df14333de5f78b63f47d9d9328
Scrounger <scrounger@gmx.net>
2020-01-09 19:02:07 +0100

    mdcInput implementation started

---
+++ widgets/materialdesign/js/widgets/materialdesign.input.js
@@ -0,0 +1,55 @@
+/*
+    ioBroker.vis vis-materialdesign Widget-Set
+
+    version: "0.2.36"
+
+    Copyright 2019 Scrounger scrounger@gmx.net
+*/
+"use strict";
+
+// this code can be placed directly in materialdesign.html
+vis.binds.materialdesign.input =
+    function (el, data) {
+        try {
+            let $this = $(el);
+
+            console.log('Vuetify Input');
+
+            $this.append(`
+            <div class="mdc-text-field" style="width: 100%; height: 100%;">
+                <input type="text" id="my-text-field" class="mdc-text-field__input">
+                <label class="mdc-floating-label" for="my-text-field">Hint text</label>
+                <div class="mdc-line-ripple"></div>
+            </div>`);
+
+            myMdwHelper.waitForElement($this, '.mdc-text-field', function () {
+                let textInput = $this.find('.mdc-text-field');
+
+                const mdcTextField = new mdc.textField.MDCTextField(textInput.get(0));
+
+                setTextInputState();
+
+                vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                    setTextInputState();
+                });
+
+                textInput.keypress(function (e) {
+                    if (e.which == 13) {
+                        vis.setValue(data.oid, mdcTextField.value);
+                    }
+                });
+
+                textInput.focusout(function () {
+                    vis.setValue(data.oid, mdcTextField.value);
+                });
+
+                function setTextInputState() {
+                    var val = vis.states.attr(data.oid + '.val');
+                    mdcTextField.value = val;
+                }
+            });
+
+        } catch (ex) {
+            console.exception(`[Input]: error: ${ex.message}, stack: ${ex.stack}`);
+        }
+    };
