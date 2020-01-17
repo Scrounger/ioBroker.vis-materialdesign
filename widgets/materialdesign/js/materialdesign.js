40f8411eac07c9ec318436775a23f065f1618d32
Scrounger <scrounger@gmx.net>
2020-01-17 20:29:40 +0100

    version for debug changed

--- widgets/materialdesign/js/materialdesign.js
+++ widgets/materialdesign/js/materialdesign.js
@@ -1,7 +1,7 @@
 /*
     ioBroker.vis vis-materialdesign Widget-Set
 
-    version: "0.2.42"
+    version: "0.2.43"
 
     Copyright 2019 Scrounger scrounger@gmx.net
 */
@@ -22,7 +22,7 @@ if (vis.editMode) {
 // TODO: move widgets to own file -> using minify
 
 vis.binds.materialdesign = {
-    version: "0.2.42",
+    version: "0.2.43",
     showVersion: function () {
         if (vis.binds["materialdesign"].version) {
             console.log('Version vis-materialdesign: ' + vis.binds["materialdesign"].version);
