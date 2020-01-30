f0d377f6c5f927a75c28da4edf388325d38f484f
Scrounger <scrounger@gmx.net>
2020-01-30 20:18:33 +0100

    version for debug changed

--- widgets/materialdesign/js/widgets/materialdesign.00.helper.js
+++ widgets/materialdesign/js/widgets/materialdesign.00.helper.js
@@ -1,7 +1,7 @@
 /*
     ioBroker.vis vis-materialdesign Widget-Set
 
-    version: "0.2.49"
+    version: "0.2.50"
 
     Copyright 2019 Scrounger scrounger@gmx.net
 */
@@ -29,7 +29,7 @@ vis.binds.materialdesign.helper = {
     },
     installedVersion: function (el, data) {
         setTimeout(function () {
-            let version = 'version: "0.2.49"'
+            let version = 'version: "0.2.50"'
             console.log(version);
             $(el).find('#versionNumber').text(version.replace('version: "', '').replace('"', ''));
         }, 1)
