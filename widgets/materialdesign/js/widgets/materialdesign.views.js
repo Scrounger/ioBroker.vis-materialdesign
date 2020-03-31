ffde20e7faac6df592211c4939d214465b1e1500
Scrounger <scrounger@gmx.net>
2020-03-31 08:51:01 +0200

    Decrepated column view removed

--- widgets/materialdesign/js/widgets/materialdesign.views.js
+++ widgets/materialdesign/js/widgets/materialdesign.views.js
@@ -8,54 +8,6 @@
 "use strict";
 
 vis.binds.materialdesign.views = {
-    column: function (el, data) {
-        try {
-            let $this = $(el);
-            let myHelper = vis.binds.materialdesign.helper;
-            let columnsCount = myMdwHelper.getNumberFromData(data.countCols, 0) + 1;
-            let columunList = [];
-            let idLastElement = '';
-
-            for (var i = 0; i <= columnsCount - 1; i++) {
-                columunList.push(`
-                    <div 
-                        class="materialdesign-column-views-container-column" 
-                        style="min-width: ${myMdwHelper.getNumberFromData(data.minWidth, 0)}px !important; width: calc(100% / ${columnsCount}) !important;">
-                        <div class="materialdesign-column-views-container-column-view">
-                `);
-
-                let viewsList = myMdwHelper.getValueFromData(data.attr('viewsInColumn' + i), '').split('|');
-                let viewsHeihgtList = myMdwHelper.getValueFromData(data.attr('viewsInColumnHeight' + i), '').split('|');
-
-                for (var view = 0; view <= viewsList.length - 1; view++) {
-                    let viewId = `col_${i}_view_${view}`;
-
-                    columunList.push(`
-                    <div 
-                        class="materialdesign-column-views-container-column-item" 
-                        id="${viewId}" 
-                        style="min-width: ${myMdwHelper.getNumberFromData(data.minWidth, 0)}px !important; width: calc(100% / ${columnsCount}) !important; height: ${myMdwHelper.getNumberFromData(viewsHeihgtList[view], 100)}px">
-                            ${(vis.editMode) ? '<div class="editmode-helper" style="border-style: dashed; border-width: 2px; border-color: #44739e;"></div>' : ''}
-                            <div data-vis-contains="${viewsList[view]}" class="vis-widget-body vis-view-container">
-                            </div>
-                    </div>
-                    `)
-
-                    idLastElement = viewId;
-                }
-                columunList.push(`</div></div>`);
-            }
-
-            $this.append(`
-                <div class="materialdesign-column-views-container">
-                    <span style="color:red"><b>Will be removed in next Version! Please use the Masnory Views Widget.</span>
-                    ${columunList.join('')}
-                </div>
-            `);
-        } catch (ex) {
-            console.error(`[Column Views ${data.wid}]: error: ${ex.message}, stack: ${ex.stack}`);
-        }
-    },
     masonry: function (el, data) {
         try {
             let $this = $(el);
