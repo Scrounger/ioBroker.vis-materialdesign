9fac1535284ab351a43807b0f105abadabd00538
Scrounger <scrounger@gmx.net>
2020-04-02 22:47:55 +0200

    select: number bug fix

--- widgets/materialdesign/js/widgets/materialdesign.00.vue.helper.js
+++ widgets/materialdesign/js/widgets/materialdesign.00.vue.helper.js
@@ -534,7 +534,7 @@ vis.binds.materialdesign.vueHelper = {
     getObjectByValue: function (val, itemsList, inputMode = '') {
         if (val !== undefined && val !== null) {
             var result = itemsList.filter(obj => {
-                return obj.value.toString() === val.toString() || parseFloat(obj.value) === parseFloat(val);
+                return obj.value.toString() === val.toString() || (!isNaN(parseFloat(obj.value)) && typeof (obj.value) === 'number' && parseFloat(obj.value) === parseFloat(val));
             });
 
             if (result.length === 1) {
