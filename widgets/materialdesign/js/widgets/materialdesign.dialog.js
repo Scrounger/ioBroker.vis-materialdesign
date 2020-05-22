d0f2114b877daa304331ff3390dce9cdc12ff530
Scrounger <scrounger@gmx.net>
2020-05-22 23:04:39 +0200

    Dialog iFrame: using vuetify lib added

--- widgets/materialdesign/js/widgets/materialdesign.dialog.js
+++ widgets/materialdesign/js/widgets/materialdesign.dialog.js
@@ -8,118 +8,7 @@
 "use strict";
 
 vis.binds.materialdesign.dialog = {
-    initialize: function (data, isIFrame = false) {
-        try {
-            let title = myMdwHelper.getValueFromData(data.title, '');
-            let titleTextSize = myMdwHelper.getFontSize(data.titleTextSize);
-            let buttonText = myMdwHelper.getValueFromData(data.buttonText, '');
-
-            return `<div class="mdc-dialog"
-                        role="alertdialog"
-                        aria-modal="true"
-                        aria-labelledby="my-dialog-title"
-                        aria-describedby="my-dialog-content"
-                        style="z-index: ${myMdwHelper.getNumberFromData(data.z_index, 1000)}">
-                        <div class="mdc-dialog__container">
-                        <div class="mdc-dialog__surface">
-                            <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
-                            <h2 class="mdc-dialog__title ${titleTextSize.class}" id="my-dialog-title" style="${(title === '') ? 'display: none;' : ''}${titleTextSize.style}" >${myMdwHelper.getValueFromData(data.title, '')}<br><span style="color:red"><b>Will be removed in next Version! Please use the new Dialog Widget!</b></span></h2>
-                            <div class="mdc-dialog__content" id="my-dialog-content">
-                                ${(vis.editMode && !isIFrame) ? `<div data-vis-contains="${data.contains_view}" class="vis-widget-body vis-view-container" style="position: relative"></div>` : ''}
-                                <!-- vis container for view generated at runtime --!>
-                                
-                                ${(isIFrame) ? `<iframe src="${data.src}" ${(data.noSandbox) ? '' : 'sandbox="allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"'} style="position: relative; border: 0; padding: 0; margin: 0; width: 100%; height: 100%; ${(data.scrollX) ? 'overflow-x: scroll;' : 'overflow-x: hidden;'} ${(data.scrollY) ? 'overflow-y: scroll;' : 'overflow-y: hidden;'}" ${(data.seamless) ? 'seamless' : ''}></iframe>` : ''}
-                            </div>
-                            <footer class="mdc-dialog__actions" ${(buttonText === '') ? 'style="display: none"' : ''}>
-                            <button type="button" class="mdc-button mdc-dialog__button" ${(!vis.editMode) ? 'data-mdc-dialog-action="close"' : ''} >
-                            <span class="mdc-button__label">${buttonText}</span>
-                            </button>
-                        </footer>
-                        </div>
-                        </div>
-                        <div class="mdc-dialog__scrim"></div>
-                    </div>`;
-        } catch (ex) {
-            console.error(`[Dialog ${data.wid}] initialize: error: ${ex.message}, stack: ${ex.stack}`);
-        }
-    },
-    handle: function (el, data, isIFrame = false) {
-        try {
-            setTimeout(function () {
-                var $this = $(el);
-                let dialog = $this.find('.mdc-dialog');
-
-                let widgetWidth = window.getComputedStyle($this.context, null).width;
-                let widgetHeight = window.getComputedStyle($this.context, null).height;
-
-                dialog.get(0).style.setProperty("--materialdesign-color-dialog-background", myMdwHelper.getValueFromData(data.colorBackground, ''));
-                dialog.get(0).style.setProperty("--materialdesign-color-dialog-title-background", myMdwHelper.getValueFromData(data.colorTitleBackground, ''));
-                dialog.get(0).style.setProperty("--materialdesign-color-dialog-title", myMdwHelper.getValueFromData(data.colorTitle, ''));
-
-                dialog.get(0).style.setProperty("--materialdesign-color-dialog-button-background", myMdwHelper.getValueFromData(data.colorButtonBackground, ''));
-                dialog.get(0).style.setProperty("--materialdesign-color-dialog-button-text", myMdwHelper.getValueFromData(data.colorButtonText, ''));
-                dialog.get(0).style.setProperty("--materialdesign-color-dialog-button-hover", myMdwHelper.getValueFromData(data.colorButtonHover, ''));
-
-                const mdcDialog = new mdc.dialog.MDCDialog(dialog.get(0));
-                const button = mdc.ripple.MDCRipple.attachTo(dialog.find('.mdc-button').get(0));
-
-                mdcDialog.escapeKeyAction = '';
-                mdcDialog.scrimClickAction = '';
-
-                if (vis.editMode && data.showInEditor) {
-                    dialog.find('.mdc-dialog__container').css('width', '100%').css('height', '100%');
-                    dialog.find('.mdc-dialog__surface').css('width', '100%').css('height', '100%');
-                    dialog.css('position', 'relative');
-                    dialog.find('.mdc-dialog__scrim').hide();
-
-                    mdcDialog.open();
-
-                } else if (!vis.editMode) {
-                    dialog.find('.mdc-dialog__container').css('width', widgetWidth.replace('px', '')).css('height', widgetHeight.replace('px', ''));
-                    dialog.find('.mdc-dialog__surface').css('width', '100%').css('height', '100%');
-
-                    if (!data.showInCenterOfScreen) {
-                        dialog.css('position', 'absolute');
-                    }
-
-                    vis.states.bind(data.showDialogOid + '.ts', function (e, newVal, oldVal) {
-                        let val = vis.states.attr(data.showDialogOid + '.val');
-
-                        if (!mdcDialog.isOpen && (val === true || val === 'true')) {
-                            mdcDialog.open();
-                        } else if (mdcDialog.isOpen && (val === false || val === 'false')) {
-                            mdcDialog.close();
-                        }
-                    });
-
-                    if (!vis.editMode) {
-                        mdcDialog.listen('MDCDialog:closing', function () {
-                            myMdwHelper.setValue(data.showDialogOid, false);
-                            vis.binds.materialdesign.helper.vibrate(data.vibrateOnMobilDevices);
-                        });
-                    }
-
-                    mdcDialog.listen('MDCDialog:opened', () => {
-                        // generate vis view
-                        if (!isIFrame) {
-                            let view = data.contains_view;
-                            if (vis.views[view]) {
-                                vis.renderView(view, view, true, function (_view) {
-                                    $('#visview_' + _view).css('position', 'relative').appendTo(dialog.find('.mdc-dialog__content')).show().data('persistent', true);
-                                });
-                            }
-                        } else {
-                            vis.binds.basic.iframeRefresh(dialog, data, this.view)
-                        }
-                    });
-                }
-            }, 1);
-
-        } catch (ex) {
-            console.error(`[Dialog ${data.wid}] handle: error: ${ex.message}, stack: ${ex.stack}`);
-        }
-    },
-    vue: function (el, data) {
+    vue: function (el, data, isIFrame = false) {
         try {
             let $this = $(el);
             let containerClass = 'materialdesign-vuetify-dialog';
@@ -154,7 +43,9 @@ vis.binds.materialdesign.dialog = {
 
                         ${myMdwHelper.getBooleanFromData(data.showTitle, false) ? `<v-card-title class="v-dialog-my-title-layout" v-html="title" v-show="!showToolbar" style="height: ${myMdwHelper.getNumberFromData(data.headerHeight, 50)}px;"></v-card-title>` : ''} 
 
-                        <v-card-text class="v-dialog-view-container" id="viewContainer_${data.wid}"></v-card-text>
+                        <v-card-text class="v-dialog-view-container" id="viewContainer_${data.wid}">
+                            ${(isIFrame) ? `<iframe class="iFrame_container" src="${data.src}" ${(data.noSandbox) ? '' : 'sandbox="allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"'} style="position: relative; border: 0; padding: 0; margin: 0; width: 100%; height: 100%; ${(data.scrollX) ? 'overflow-x: scroll;' : 'overflow-x: hidden;'} ${(data.scrollY) ? 'overflow-y: scroll;' : 'overflow-y: hidden;'}" ${(data.seamless) ? 'seamless' : ''}></iframe>` : ''}
+                        </v-card-text>
 
                         <div class="v-dialog-footer" v-show="!showToolbar" style="height: ${myMdwHelper.getNumberFromData(data.footerHeight, myMdwHelper.getBooleanFromData(data.showTitle, false) ? 61 : 56)}px">
                             ${myMdwHelper.getBooleanFromData(data.showDivider, false) ? `<div style="width: 100%; height: 1px; background: ${myMdwHelper.getValueFromData(data.dividerColor, 'lightgray')}; margin-top: 4px;"></div>` : ''}
@@ -262,11 +153,16 @@ vis.binds.materialdesign.dialog = {
 
                             let view = data.contains_view;
                             if ($dialog.find('#visview_' + view).length < 1) {
-                                if (vis.views[view]) {
-                                    vis.renderView(view, view, true, function (_view) {
-                                        $('#visview_' + _view).css('position', 'relative').css('height', wishHeight + 'px').appendTo($dialog.find(`#viewContainer_${data.wid}`)).show().data('persistent', true);
-                                    });
+                                if (!isIFrame) {
+                                    if (vis.views[view]) {
+                                        vis.renderView(view, view, true, function (_view) {
+                                            $('#visview_' + _view).css('position', 'relative').css('height', wishHeight + 'px').appendTo($dialog.find(`#viewContainer_${data.wid}`)).show().data('persistent', true);
+                                        });
+                                    }
+                                } else {
+                                    vis.binds.basic.iframeRefresh($dialog, data, view)
                                 }
+
                             }
                         });
                     }
@@ -307,7 +203,11 @@ vis.binds.materialdesign.dialog = {
                             $dialog.get(0).style.setProperty("--vue-ripple-effect-color", myMdwHelper.getValueFromData(data.pressColor, ''));
                         }
 
-                        $dialog.find('#visview_' + data.contains_view).css('height', wishHeight + 'px');
+                        if (!isIFrame) {
+                            $dialog.find('#visview_' + data.contains_view).css('height', wishHeight + 'px');
+                        } else {
+                            $dialog.find('.iFrame_container').css('height', (wishHeight - 2) + 'px');
+                        }
                     }
                 });
             });
