35fc91e601a8fccc242edd39b33fceed066da641
Scrounger <scrounger@gmx.net>
2020-06-10 11:26:49 +0200

    button vertical: style bug fix

--- widgets/materialdesign/js/widgets/materialdesign.button.js
+++ widgets/materialdesign/js/widgets/materialdesign.button.js
@@ -84,7 +84,7 @@ vis.binds.materialdesign.button = {
             let labelElement = '';
             if (myMdwHelper.getValueFromData(data.buttontext, null) != null) {
                 labelElement = `<span 
-                                    class="materialdesign-button__label" style="text-align: center; margin-top: 10px;">
+                                    class="materialdesign-button__label" style="text-align: center;">
                                     ${data.buttontext}
                                 </span>`;
             }
