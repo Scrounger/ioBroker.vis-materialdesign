1e9d7a7742a29482e2cee2b68faac6739c7e415e
Scrounger <scrounger@gmx.net>
2021-01-25 17:45:42 +0100

    alerts: using subscribed states for theme instead of binding

--- widgets/materialdesign/js/widgets/materialdesign.alerts.js
+++ widgets/materialdesign/js/widgets/materialdesign.alerts.js
@@ -10,72 +10,46 @@ var myMdwAlertClearButtonClicked = false;
 // this code can be placed directly in materialdesign.html
 vis.binds.materialdesign.alerts =
     function (el, data) {
+        let widgetName = 'Alerts';
+        let themeTriggerClass = '.materialdesign-widget.materialdesign-alerts'
+
         try {
             let $this = $(el);
-            let idPrefix = "alerts_";
-            let vueHelper = vis.binds.materialdesign.vueHelper.alerts;
-            let vueAlertElements = [];
 
-            let jsonData = null;
-            try {
-                jsonData = JSON.parse(vis.states.attr(data.oid + '.val'));
+            myMdwHelper.subscribeThemesAtRuntimee(data, widgetName, themeTriggerClass, function () {
+                init();
+            });
 
-                if (vis.editMode && jsonData.length === 0) {
-                    jsonData = jsonHasNoAlerts();
-                    // Dummy alerts im Editor anzeigen, falls keine vorhanden
+            function init() {
+                let idPrefix = "alerts_";
+                let vueHelper = vis.binds.materialdesign.vueHelper.alerts;
+                let vueAlertElements = [];
+
+                let jsonData = null;
+                try {
+                    jsonData = JSON.parse(vis.states.attr(data.oid + '.val'));
+
+                    if (vis.editMode && jsonData.length === 0) {
+                        jsonData = jsonHasNoAlerts();
+                        // Dummy alerts im Editor anzeigen, falls keine vorhanden
+                    }
+                } catch (err) {
+                    jsonData = jsonHasErrorAlert();
+                    console.error(`[Vuetify Alerts 1 - ${data.wid}] cannot parse json string! Error: ${err.message}`);
                 }
-            } catch (err) {
-                jsonData = jsonHasErrorAlert();
-                console.error(`[Vuetify Alerts 1 - ${data.wid}] cannot parse json string! Error: ${err.message}`);
-            }
 
-            if (jsonData !== null) {
-                for (var i = 0; i <= jsonData.length - 1; i++) {
-                    vueHelper.generateElement(data, $this, idPrefix, i);
-                }
-
-                // myMdwHelper.waitForElement($this, `#${idPrefix}${jsonData.length - 1}`, function () {
-                myMdwHelper.waitForElement($("body"), '#materialdesign-vuetify-container', data.wid, 'Alerts', function () {
-
+                if (jsonData !== null) {
                     for (var i = 0; i <= jsonData.length - 1; i++) {
-                        let item = jsonData[i];
-                        item.id = `${idPrefix}${i}`
-
-                        vueAlertElements.push(vueHelper.getVuetifyElement($this, item, idPrefix, i, data));
+                        vueHelper.generateElement(data, $this, idPrefix, i);
                     }
 
-                    let iconButtons = $this.find('.materialdesign-icon-button');
-                    for (var b = 0; b <= iconButtons.length - 1; b++) {
-                        // set ripple effect to icon buttons
-                        new mdc.iconButton.MDCIconButtonToggle($this.find('.materialdesign-icon-button').get(b));
-                    }
+                    // myMdwHelper.waitForElement($this, `#${idPrefix}${jsonData.length - 1}`, function () {
+                    myMdwHelper.waitForElement($("body"), '#materialdesign-vuetify-container', data.wid, 'Alerts', function () {
 
-
-                    vueHelper.initializeClearButtonEvent($this, vueAlertElements, data, jsonData, idPrefix);
-                });
-                // });
-            }
-
-            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
-                if (!myMdwAlertClearButtonClicked) {
-                    // first clear all
-                    jsonData === null;
-                    $this.find('.v-alert').remove();
-                    vueAlertElements = [];
-
-                    try {
-                        jsonData = JSON.parse(vis.states.attr(data.oid + '.val'));
-                    } catch (err) {
-                        jsonData = jsonHasErrorAlert();
-                        console.error(`[Vuetify Alerts 2 - ${data.wid}] cannot parse json string! Error: ${err.message}`);
-                    }
-
-                    if (jsonData !== null) {
                         for (var i = 0; i <= jsonData.length - 1; i++) {
                             let item = jsonData[i];
                             item.id = `${idPrefix}${i}`
 
-                            vueHelper.generateElement(data, $this, idPrefix, i);
                             vueAlertElements.push(vueHelper.getVuetifyElement($this, item, idPrefix, i, data));
                         }
 
@@ -85,63 +59,115 @@ vis.binds.materialdesign.alerts =
                             new mdc.iconButton.MDCIconButtonToggle($this.find('.materialdesign-icon-button').get(b));
                         }
 
+
                         vueHelper.initializeClearButtonEvent($this, vueAlertElements, data, jsonData, idPrefix);
-                    }
+                    });
+                    // });
                 }
 
-                myMdwAlertClearButtonClicked = false;
-            });
+                vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                    if (!myMdwAlertClearButtonClicked) {
+                        // first clear all
+                        jsonData === null;
+                        $this.find('.v-alert').remove();
+                        vueAlertElements = [];
 
-            $this.context.style.setProperty("--vue-alerts-button-close-color", myMdwHelper.getValueFromData(data.closeIconColor, ''));
-            $this.context.style.setProperty("--mdc-theme-primary", myMdwHelper.getValueFromData(data.closeIconPressColor, ''));
+                        try {
+                            jsonData = JSON.parse(vis.states.attr(data.oid + '.val'));
+                        } catch (err) {
+                            jsonData = jsonHasErrorAlert();
+                            console.error(`[Vuetify Alerts 2 - ${data.wid}] cannot parse json string! Error: ${err.message}`);
+                        }
 
-            $this.context.style.setProperty("--vue-alerts-text-font-family", myMdwHelper.getValueFromData(data.alertFontFamily, 'inherit'));
-            $this.context.style.setProperty("--vue-alerts-text-size", myMdwHelper.getNumberFromData(data.alertFontSize, '16') + 'px');
+                        if (jsonData !== null) {
+                            for (var i = 0; i <= jsonData.length - 1; i++) {
+                                let item = jsonData[i];
+                                item.id = `${idPrefix}${i}`
 
-            $this.context.style.setProperty("--vue-alerts-icon-size", myMdwHelper.getNumberFromData(data.alertIconSize, '24') + 'px');
+                                vueHelper.generateElement(data, $this, idPrefix, i);
+                                vueAlertElements.push(vueHelper.getVuetifyElement($this, item, idPrefix, i, data));
+                            }
 
-            $this.context.style.setProperty("--vue-alerts-bottom-margin", myMdwHelper.getNumberFromData(data.alertMarginBottom, '16') + 'px');
+                            let iconButtons = $this.find('.materialdesign-icon-button');
+                            for (var b = 0; b <= iconButtons.length - 1; b++) {
+                                // set ripple effect to icon buttons
+                                new mdc.iconButton.MDCIconButtonToggle($this.find('.materialdesign-icon-button').get(b));
+                            }
 
-            if ($(window).width() < data.minScreenResolution) {
-                $this.hide();
-            }
+                            vueHelper.initializeClearButtonEvent($this, vueAlertElements, data, jsonData, idPrefix);
+                        }
+                    }
 
-            $(window).resize(function () {
+                    myMdwAlertClearButtonClicked = false;
+                });
+
+                vis.states.bind('vis-materialdesign.0.colors.darkTheme.val', function (e, newVal, oldVal) {
+                    setLayout();
+                });
+
+                vis.states.bind('vis-materialdesign.0.lastchange.val', function (e, newVal, oldVal) {
+                    setLayout();
+                });
+
+                $(themeTriggerClass).on(`mdwTheme_subscribe_${widgetName.replace(/ /g, '_')}`, function () {
+                    if (data.debug) console.log(`[${widgetName} - ${data.wid}] event received: 'mdwTheme_subscribe_${widgetName.replace(/ /g, '_')}'`);
+                    setLayout();
+                });
+
+                setLayout();
+                function setLayout() {
+                    $this.context.style.setProperty("--vue-alerts-button-close-color", myMdwHelper.getValueFromData(data.closeIconColor, ''));
+                    $this.context.style.setProperty("--materialdesign-color-icon-button-hover", myMdwHelper.getValueFromData(data.closeIconPressColor, ''));
+
+                    $this.context.style.setProperty("--vue-alerts-text-font-family", myMdwHelper.getValueFromData(data.alertFontFamily, 'inherit'));
+                    $this.context.style.setProperty("--vue-alerts-text-size", myMdwHelper.getNumberFromData(data.alertFontSize, '16') + 'px');
+
+                    $this.context.style.setProperty("--vue-alerts-icon-size", myMdwHelper.getNumberFromData(data.alertIconSize, '24') + 'px');
+
+                    $this.context.style.setProperty("--vue-alerts-bottom-margin", myMdwHelper.getNumberFromData(data.alertMarginBottom, '16') + 'px');
+                }
+
                 if ($(window).width() < data.minScreenResolution) {
                     $this.hide();
-                }else{
-                    $this.show();
                 }
-            });
 
-            function jsonHasErrorAlert() {
-                return [
-                    {
-                        "text": _("Error in JSON string"),
-                        "borderColor": "red",
-                        "icon": "alert-box",
-                        "iconColor": "red"
+                $(window).resize(function () {
+                    if ($(window).width() < data.minScreenResolution) {
+                        $this.hide();
+                    } else {
+                        $this.show();
                     }
-                ]
-            }
+                });
 
-            function jsonHasNoAlerts() {
-                return [
-                    {
-                        "text": _("example that is only displayed in the editor"),
-                        "borderColor": "blue",
-                        "icon": "home",
-                        "iconColor": "blue"
-                    },
-                    {
-                        "text": _("no alert messages currently available"),
-                        "borderColor": "orange",
-                        "icon": "information",
-                        "iconColor": "orange"
-                    }
-                ]
+                function jsonHasErrorAlert() {
+                    return [
+                        {
+                            "text": _("Error in JSON string"),
+                            "borderColor": "red",
+                            "icon": "alert-box",
+                            "iconColor": "red"
+                        }
+                    ]
+                }
+
+                function jsonHasNoAlerts() {
+                    return [
+                        {
+                            "text": _("example that is only displayed in the editor"),
+                            "borderColor": "blue",
+                            "icon": "home",
+                            "iconColor": "blue"
+                        },
+                        {
+                            "text": _("no alert messages currently available"),
+                            "borderColor": "orange",
+                            "icon": "information",
+                            "iconColor": "orange"
+                        }
+                    ]
+                }
             }
         } catch (ex) {
-            console.error(`[Vuetify Alerts - ${data.wid}]: error: ${ex.message}, stack: ${ex.stack} `);
+            console.error(`[${widgetName} - ${data.wid}]: error: ${ex.message}, stack: ${ex.stack} `);
         }
     };
