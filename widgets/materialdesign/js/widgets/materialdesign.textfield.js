4e9cc996e5041cd28a7a04ce6f7ae5440721955b
Scrounger <scrounger@gmx.net>
2021-01-27 18:34:33 +0100

    input: using subscribed states for theme instead of binding

--- widgets/materialdesign/js/widgets/materialdesign.textfield.js
+++ widgets/materialdesign/js/widgets/materialdesign.textfield.js
@@ -8,32 +8,41 @@
 // this code can be placed directly in materialdesign.html
 vis.binds.materialdesign.textfield = {
     initialize: function (el, data) {
+        let widgetName = 'TextField';
+        let themeTriggerClass = '.materialdesign-widget .materialdesign-input';
+
         try {
             let $this = $(el);
             let vueHelper = vis.binds.materialdesign.vueHelper.input
             let containerClass = 'materialdesign-vuetify-textField';
-            let widgetName = 'TextField';
 
-            let inputType = myMdwHelper.getValueFromData(data.inputType, 'text');
-            let inputMask = '';
-            let placeholder = ''
+            myMdwHelper.subscribeThemesAtRuntimee(data, widgetName, themeTriggerClass, function () {
+                init();
+            });
 
-            if (inputType === 'mask') {
-                // mask needs text as input type
-                inputType = 'text';
+            function init() {
 
-                if (data.inputMask) {
-                    if (data.inputMask.startsWith('[') && data.inputMask.endsWith(']')) {
-                        inputMask = `v-mask="${data.inputMask}"`
-                        placeholder = data.inputMask.replace('[', '').replace(']', '').replace("'", '');
-                    } else {
-                        inputMask = `v-mask="'${myMdwHelper.getValueFromData(data.inputMask, '')}'"`
-                        placeholder = data.inputMask;
+
+                let inputType = myMdwHelper.getValueFromData(data.inputType, 'text');
+                let inputMask = '';
+                let placeholder = ''
+
+                if (inputType === 'mask') {
+                    // mask needs text as input type
+                    inputType = 'text';
+
+                    if (data.inputMask) {
+                        if (data.inputMask.startsWith('[') && data.inputMask.endsWith(']')) {
+                            inputMask = `v-mask="${data.inputMask}"`
+                            placeholder = data.inputMask.replace('[', '').replace(']', '').replace("'", '');
+                        } else {
+                            inputMask = `v-mask="'${myMdwHelper.getValueFromData(data.inputMask, '')}'"`
+                            placeholder = data.inputMask;
+                        }
                     }
                 }
-            }
 
-            $this.append(`
+                $this.append(`
             <div class="${containerClass}" style="width: 100%; height: 100%;">
                 <v-text-field
                     v-model="value"
@@ -45,77 +54,91 @@ vis.binds.materialdesign.textfield = {
                 ${vueHelper.getTemplates(data)}
 
                 ${(myMdwHelper.getValueFromData(data.appendIcon, null) !== null) ?
-                    `<template v-slot:append>
+                        `<template v-slot:append>
                         <div  class="v-input__icon v-input__icon--append">                            
                             <v-icon v-if="appendIcon !== ''">{{ appendIcon }}</v-icon>
                             <img v-if="appendImage !== ''" :src="appendImage" />
                         </div>
                     </template>`
-                    : ''}                
+                        : ''}                
 
                 </v-text-field>
             </div>`);
 
-            if (myMdwHelper.oidNeedSubscribe(data.oid, data.wid, widgetName, false)) {
-                // ggf subscribing notwendig, wenn z.B. Binding als ObjektId verwendet wird und eine oid übergeben wird
-                myMdwHelper.subscribeStatesAtRuntime(data.wid, widgetName, function () {
+                if (myMdwHelper.oidNeedSubscribe(data.oid, data.wid, widgetName, false)) {
+                    // ggf subscribing notwendig, wenn z.B. Binding als ObjektId verwendet wird und eine oid übergeben wird
+                    myMdwHelper.subscribeStatesAtRuntime(data.wid, widgetName, function () {
+                        handler();
+                    });
+                } else {
                     handler();
-                });
-            } else {
-                handler();
-            }
+                }
 
-            function handler() {
-                myMdwHelper.waitForElement($this, `.${containerClass}`, data.wid, 'TextField', function () {
-                    myMdwHelper.waitForElement($("body"), '#materialdesign-vuetify-container', data.wid, 'TextField', function () {
+                function handler() {
+                    myMdwHelper.waitForElement($this, `.${containerClass}`, data.wid, 'TextField', function () {
+                        myMdwHelper.waitForElement($("body"), '#materialdesign-vuetify-container', data.wid, 'TextField', function () {
 
-                        let widgetHeight = window.getComputedStyle($this.get(0), null).height.replace('px', '');
+                            let widgetHeight = window.getComputedStyle($this.get(0), null).height.replace('px', '');
 
-                        Vue.use(VueTheMask);
-                        let vueTextField = new Vue({
-                            el: $this.find(`.${containerClass}`).get(0),
-                            vuetify: new Vuetify(),
-                            data() {
-                                let dataObj = vueHelper.getData(data, widgetHeight, placeholder);
+                            Vue.use(VueTheMask);
+                            let vueTextField = new Vue({
+                                el: $this.find(`.${containerClass}`).get(0),
+                                vuetify: new Vuetify(),
+                                data() {
+                                    let dataObj = vueHelper.getData(data, widgetHeight, placeholder);
 
-                                dataObj.value = vis.states.attr(data.oid + '.val');
-                                dataObj.type = inputType;
-                                dataObj.maxlength = myMdwHelper.getNumberFromData(data.inputMaxLength, '');
+                                    dataObj.value = vis.states.attr(data.oid + '.val');
+                                    dataObj.type = inputType;
+                                    dataObj.maxlength = myMdwHelper.getNumberFromData(data.inputMaxLength, '');
 
-                                dataObj.appendIcon = (myMdwHelper.getAllowedImageFileExtensions().some(el => myMdwHelper.getValueFromData(data.appendIcon, '').includes(el))) ? undefined : myMdwHelper.getValueFromData(data.appendIcon, undefined, 'mdi-');
-                                dataObj.appendImage = (myMdwHelper.getAllowedImageFileExtensions().some(el => myMdwHelper.getValueFromData(data.appendIcon, '').includes(el))) ? myMdwHelper.getValueFromData(data.appendIcon, undefined) : undefined;
+                                    dataObj.appendIcon = (myMdwHelper.getAllowedImageFileExtensions().some(el => myMdwHelper.getValueFromData(data.appendIcon, '').includes(el))) ? undefined : myMdwHelper.getValueFromData(data.appendIcon, undefined, 'mdi-');
+                                    dataObj.appendImage = (myMdwHelper.getAllowedImageFileExtensions().some(el => myMdwHelper.getValueFromData(data.appendIcon, '').includes(el))) ? myMdwHelper.getValueFromData(data.appendIcon, undefined) : undefined;
 
-                                return dataObj;
-                            },
-                            methods: {
-                                changeEvent(value) {
-                                    if (inputType !== 'number') {
-                                        myMdwHelper.setValue(data.oid, value);
-                                    } else {
-                                        if (value) {
+                                    return dataObj;
+                                },
+                                methods: {
+                                    changeEvent(value) {
+                                        if (inputType !== 'number') {
                                             myMdwHelper.setValue(data.oid, value);
                                         } else {
-                                            this.value = vis.states.attr(data.oid + '.val');
+                                            if (value) {
+                                                myMdwHelper.setValue(data.oid, value);
+                                            } else {
+                                                this.value = vis.states.attr(data.oid + '.val');
+                                            }
                                         }
                                     }
                                 }
-                            }
-                        });
+                            });
 
-                        vueHelper.setStyles($this, data);
+                            vis.states.bind('vis-materialdesign.0.colors.darkTheme.val', function (e, newVal, oldVal) {
+                                vueHelper.setStyles($this, data);
+                            });
+    
+                            vis.states.bind('vis-materialdesign.0.lastchange.val', function (e, newVal, oldVal) {
+                                vueHelper.setStyles($this, data);
+                            });
+    
+                            $(themeTriggerClass).on(`mdwTheme_subscribe_${widgetName.replace(/ /g, '_')}`, function () {
+                                if (data.debug) console.log(`[${widgetName} - ${data.wid}] event received: 'mdwTheme_subscribe_${widgetName.replace(/ /g, '_')}'`);
+                                vueHelper.setStyles($this, data);
+                            });
 
-                        // Append Icon
-                        $this.get(0).style.setProperty("--vue-text-icon-append-size", myMdwHelper.getNumberFromData(data.appendIconSize, 16) + 'px');
-                        $this.get(0).style.setProperty("--vue-text-icon-append-color", myMdwHelper.getValueFromData(data.appendIconColor, ''));
+                            vueHelper.setStyles($this, data);
 
-                        vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
-                            vueTextField.value = newVal;
+                            // Append Icon
+                            $this.get(0).style.setProperty("--vue-text-icon-append-size", myMdwHelper.getNumberFromData(data.appendIconSize, 16) + 'px');
+                            $this.get(0).style.setProperty("--vue-text-icon-append-color", myMdwHelper.getValueFromData(data.appendIconColor, ''));
+
+                            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                                vueTextField.value = newVal;
+                            });
                         });
                     });
-                });
+                }
             }
         } catch (ex) {
-            console.error(`[TextField - ${data.wid}]: error: ${ex.message}, stack: ${ex.stack} `);
+            console.error(`[${widgetName} - ${data.wid}]: error: ${ex.message}, stack: ${ex.stack} `);
         }
     },
     getDataFromJson(obj, widgetId) {
