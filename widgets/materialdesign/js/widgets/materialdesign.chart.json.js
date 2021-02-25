77056205b9dffaee0405fe576f200604dfb43c6e
Scrounger <scrounger@gmx.net>
2021-02-25 21:53:56 +0100

    json chart: null bug fix

--- widgets/materialdesign/js/widgets/materialdesign.chart.json.js
+++ widgets/materialdesign/js/widgets/materialdesign.chart.json.js
@@ -206,7 +206,9 @@ vis.binds.materialdesign.chart.json = function (el, data) {
                                                         rotation: myMdwHelper.getNumberFromData(graph.datalabel_rotation, undefined),
                                                         formatter: function (value, context) {
                                                             if (myMdwHelper.getValueFromData(graph.datalabel_override, undefined)) {
-                                                                return graph.datalabel_override[context.dataIndex].split('\\n');
+                                                                if (graph.datalabel_override[context.dataIndex]) {
+                                                                    return graph.datalabel_override[context.dataIndex].split('\\n');
+                                                                }
                                                             } else {
                                                                 if (!isTimeAxis) {
                                                                     if ((value || value === 0) && context.dataIndex % myMdwHelper.getNumberFromData(graph.datalabel_steps, 1) === 0) {
