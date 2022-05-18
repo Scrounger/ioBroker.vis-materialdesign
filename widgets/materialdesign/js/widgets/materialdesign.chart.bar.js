0baa8eac116aba56002b9bf052e20716d22e641e
Scrounger <scrounger@gmx.net>
2022-05-18 12:56:18 +0200

    bug fixes

--- widgets/materialdesign/js/widgets/materialdesign.chart.bar.js
+++ widgets/materialdesign/js/widgets/materialdesign.chart.bar.js
@@ -42,21 +42,23 @@ vis.binds.materialdesign.chart = {
 
                 setLayout();
                 function setLayout(changed = false) {
-                    $this.context.style.setProperty("--materialdesign-font-card-title", myChartHelper.getValueFromData(data.titleFontFamily, ''));
+                    myMdwHelper.waitForCssVariable(function () {
+                        $this.context.style.setProperty("--materialdesign-font-card-title", myChartHelper.getValueFromData(data.titleFontFamily, ''));
 
-                    $this.context.style.setProperty("--materialdesign-color-card-background", myChartHelper.getValueFromData(data.colorBackground, ''));
-                    $this.context.style.setProperty("--materialdesign-color-card-title-section-background", myChartHelper.getValueFromData(data.colorTitleSectionBackground, ''));
-                    $this.context.style.setProperty("--materialdesign-color-card-text-section-background", myChartHelper.getValueFromData(data.colorTextSectionBackground, ''));
-                    $this.context.style.setProperty("--materialdesign-color-card-title", myChartHelper.getValueFromData(data.colorTitle, ''));
+                        $this.context.style.setProperty("--materialdesign-color-card-background", myChartHelper.getValueFromData(data.colorBackground, ''));
+                        $this.context.style.setProperty("--materialdesign-color-card-title-section-background", myChartHelper.getValueFromData(data.colorTitleSectionBackground, ''));
+                        $this.context.style.setProperty("--materialdesign-color-card-text-section-background", myChartHelper.getValueFromData(data.colorTextSectionBackground, ''));
+                        $this.context.style.setProperty("--materialdesign-color-card-title", myChartHelper.getValueFromData(data.colorTitle, ''));
 
-                    let titleFontSize = myChartHelper.getFontSize(data.titleLayout);
-                    if (titleFontSize && titleFontSize.style) {
-                        $this.find('.card-title').css('font-size', myChartHelper.getStringFromNumberData(data.titleLayout, 'inherit', '', 'px'));
-                    }
+                        let titleFontSize = myChartHelper.getFontSize(data.titleLayout);
+                        if (titleFontSize && titleFontSize.style) {
+                            $this.find('.card-title').css('font-size', myChartHelper.getStringFromNumberData(data.titleLayout, 'inherit', '', 'px'));
+                        }
 
-                    if (changed) {
-                        createChart();
-                    }
+                        if (changed) {
+                            createChart();
+                        }
+                    }, 0, data.debug);
                 }
 
                 createChart();
@@ -264,86 +266,88 @@ vis.binds.materialdesign.chart = {
                         myBarChart.update();
 
                         function onChange(e, newVal, oldVal) {
-                            // i wird nicht gespeichert -> umweg über oid gehen, um index zu erhalten
-                            try {
-                                if (data.chartDataMethod === 'inputPerEditor') {
-                                    let oidId = e.type.substr(0, e.type.lastIndexOf("."));
+                            myMdwHelper.waitForCssVariable(function () {
+                                // i wird nicht gespeichert -> umweg über oid gehen, um index zu erhalten
+                                try {
+                                    if (data.chartDataMethod === 'inputPerEditor') {
+                                        let oidId = e.type.substr(0, e.type.lastIndexOf("."));
 
-                                    for (var d = 0; d <= data.dataCount; d++) {
-                                        if (oidId === data.attr('oid' + d)) {
-                                            let index = d;
-                                            myBarChart.data.datasets[0].data[index] = newVal;
+                                        for (var d = 0; d <= data.dataCount; d++) {
+                                            if (oidId === data.attr('oid' + d)) {
+                                                let index = d;
+                                                myBarChart.data.datasets[0].data[index] = newVal;
+                                                myBarChart.update();
+                                            }
+                                        }
+                                    } else {
+                                        let jsonData = undefined;
+
+                                        try {
+                                            jsonData = JSON.parse(newVal);
+                                        } catch (errJson) {
+                                            myBarChart.options.title = {
+                                                display: true,
+                                                text: `${_("Error in JSON string")}<br>${errJson.message}`.split('<br>'),
+                                                fontColor: 'red'
+                                            };
+                                            myBarChart.update();
+
+                                            console.error(`[Bar Chart - ${data.wid}] onChange: cannot parse json string! Error: ${errJson.message}`);
+                                        }
+
+                                        if (jsonData && jsonData.length > 0) {
+                                            // first remove old data, because count of items could changed
+                                            myBarChart.data.datasets[0].data = [];
+                                            myBarChart.data.datasets[0].backgroundColor = [];
+                                            myBarChart.data.labels = [];
+                                            myBarChart.data.datasets[0].hoverBackgroundColor = [];
+                                            myBarChart.options.plugins.datalabels.color = [];
+
+                                            for (var d = 0; d <= jsonData.length - 1; d++) {
+                                                if (colorScheme !== null) {
+                                                    globalColor = colorScheme[d];
+                                                }
+
+                                                let barItem = getBarItemObj(d, data, jsonData, globalColor, globalValueTextColor);
+
+                                                if (barItem) {
+                                                    myBarChart.data.datasets[0].data[d] = barItem.value;
+                                                    myBarChart.data.datasets[0].backgroundColor[d] = barItem.dataColor;
+                                                    myBarChart.data.labels[d] = barItem.label;
+
+                                                    if (myChartHelper.getValueFromData(data.hoverColor, null) === null) {
+                                                        myBarChart.data.datasets[0].hoverBackgroundColor[d] = myChartHelper.addOpacityToColor(barItem.dataColor, 80);
+                                                    } else {
+                                                        myBarChart.data.datasets[0].hoverBackgroundColor[d] = data.hoverColor;
+                                                    }
+
+                                                    myBarChart.options.plugins.datalabels.color[d] = barItem.valueColor;
+
+                                                    myBarChart.options.plugins.datalabels.formatter = function (value, context) {
+                                                        if (value) {
+                                                            let barItem = getBarItemObj(context.dataIndex, data, jsonData, globalColor, globalValueTextColor, value);
+                                                            if (barItem) {
+                                                                return `${barItem.valueText}${barItem.valueAppendix}`.split('\\n');
+                                                            }
+                                                        }
+                                                        return '';
+                                                    }
+                                                }
+                                            }
                                             myBarChart.update();
                                         }
                                     }
-                                } else {
-                                    let jsonData = undefined;
+                                } catch (err) {
+                                    myBarChart.options.title = {
+                                        display: true,
+                                        text: `onChange: error: ${err.message}, stack: ${err.message}`,
+                                        fontColor: 'red'
+                                    };
+                                    myBarChart.update();
 
-                                    try {
-                                        jsonData = JSON.parse(newVal);
-                                    } catch (errJson) {
-                                        myBarChart.options.title = {
-                                            display: true,
-                                            text: `${_("Error in JSON string")}<br>${errJson.message}`.split('<br>'),
-                                            fontColor: 'red'
-                                        };
-                                        myBarChart.update();
-
-                                        console.error(`[Bar Chart - ${data.wid}] onChange: cannot parse json string! Error: ${errJson.message}`);
-                                    }
-
-                                    if (jsonData && jsonData.length > 0) {
-                                        // first remove old data, because count of items could changed
-                                        myBarChart.data.datasets[0].data = [];
-                                        myBarChart.data.datasets[0].backgroundColor = [];
-                                        myBarChart.data.labels = [];
-                                        myBarChart.data.datasets[0].hoverBackgroundColor = [];
-                                        myBarChart.options.plugins.datalabels.color = [];
-
-                                        for (var d = 0; d <= jsonData.length - 1; d++) {
-                                            if (colorScheme !== null) {
-                                                globalColor = colorScheme[d];
-                                            }
-
-                                            let barItem = getBarItemObj(d, data, jsonData, globalColor, globalValueTextColor);
-
-                                            if (barItem) {
-                                                myBarChart.data.datasets[0].data[d] = barItem.value;
-                                                myBarChart.data.datasets[0].backgroundColor[d] = barItem.dataColor;
-                                                myBarChart.data.labels[d] = barItem.label;
-
-                                                if (myChartHelper.getValueFromData(data.hoverColor, null) === null) {
-                                                    myBarChart.data.datasets[0].hoverBackgroundColor[d] = myChartHelper.addOpacityToColor(barItem.dataColor, 80);
-                                                } else {
-                                                    myBarChart.data.datasets[0].hoverBackgroundColor[d] = data.hoverColor;
-                                                }
-
-                                                myBarChart.options.plugins.datalabels.color[d] = barItem.valueColor;
-
-                                                myBarChart.options.plugins.datalabels.formatter = function (value, context) {
-                                                    if (value) {
-                                                        let barItem = getBarItemObj(context.dataIndex, data, jsonData, globalColor, globalValueTextColor, value);
-                                                        if (barItem) {
-                                                            return `${barItem.valueText}${barItem.valueAppendix}`.split('\\n');
-                                                        }
-                                                    }
-                                                    return '';
-                                                }
-                                            }
-                                        }
-                                        myBarChart.update();
-                                    }
+                                    console.error(`[Bar Chart - ${data.wid}] onChange: error: ${err.message}, stack: ${err.message}`);
                                 }
-                            } catch (err) {
-                                myBarChart.options.title = {
-                                    display: true,
-                                    text: `onChange: error: ${err.message}, stack: ${err.message}`,
-                                    fontColor: 'red'
-                                };
-                                myBarChart.update();
-
-                                console.error(`[Bar Chart - ${data.wid}] onChange: error: ${err.message}, stack: ${err.message}`);
-                            }
+                            }, 0, data.debug);
                         }
 
                         function getBarItemObj(i, data, jsonData, globalColor, globalValueTextColor, value = 0) {
